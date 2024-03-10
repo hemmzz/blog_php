1b7e3163fce279938f2ec16b8b4c83af55510855
Emmanuel Isaiah <emmanuelisaiah2001@gmail.com>
2024-03-10 22:43:51 +0000

    Create registration page for admin

---
+++ admin/admin_reg.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Admin Registration</title>
+</head>
+<body class="">
+    <header>
+        <nav class="navbar navbar-light bg-light">
+            <a href="" class="navbar-brand">Blog</a>
+        </nav>
+    </header>
+    <main class="mt-5 container-fluid">
+        <div class="row">
+            <div class="col-md-4"></div>
+
+            <div class="col-md-4 shadow-lg">
+                <form action="" method="post">
+                    <label for="lastname" class="mt-2">Enter Lastname</label>
+                    <input type="text" class="form-control form-control-sm"><br>
+
+                    <label for="firstname">Enter Firstname</label>
+                    <input type="text" class="form-control form-control-sm"><br>
+
+                    <label for="username">Create Username</label>
+                    <input type="text" class="form-control form-control-sm"><br>
+
+                    <label for="email">Enter Email</label>
+                    <input type="text" class="form-control form-control-sm"><br>
+
+                    <label for="password">Create Passwords</label>
+                    <input type="password" class="form-control form-control-sm"><br>
+
+                    <label for="confirm_password">Confirm Password</label>
+                    <input type="password" class="form-control form-control-sm"><br>
+
+                    <button class="btn btn-success mb-2">Sign Up</button>
+                </form>
+            </div>
+
+            <div class="col-md-4"></div>
+        </div>
+    </main>
+</body>
+</html>
