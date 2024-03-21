6689f072a6ce983da52d9389cae21fac282867ed
Emmanuel Isaiah <emmanuelisaiah2001@gmail.com>
2024-03-21 20:06:41 +0000

    Style admin dashboard and make create post file

--- admin/admin_dashboard.php
+++ admin/admin_dashboard.php
@@ -1,4 +1,14 @@
-<!doctype html>
+<?php
+
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+	header("Location: login.php");
+}
+
+
+
+?><!doctype html>
 <html lang="en">
   <head>
   	<title>Sidebar 02</title>
@@ -8,7 +18,9 @@
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
 		
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/second-style.css">
+    <link rel="stylesheet" href="../scss/bootstrap/_variables.scss">
   </head>
   <body>
 		
@@ -21,60 +33,40 @@
 	        </button>
         </div>
 				<div class="p-4 pt-5">
-		  		<h1><a href="index.html" class="logo">Splash</a></h1>
+		  		<h1><a href="index.html" class="logo">Blog</a></h1>
 	        <ul class="list-unstyled components mb-5">
 	          <li class="active">
 	            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
 	            <ul class="collapse list-unstyled" id="homeSubmenu">
                 <li>
-                    <a href="#">Home 1</a>
+                    <a href="#">Create Post</a>
                 </li>
                 <li>
-                    <a href="#">Home 2</a>
+                    <a href="#">Manage Post</a>
                 </li>
                 <li>
-                    <a href="#">Home 3</a>
+                    <a href="#"></a>
                 </li>
 	            </ul>
 	          </li>
 	          <li>
-	              <a href="#">About</a>
+	              <a href="#">Send Notification</a>
 	          </li>
 	          <li>
-              <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
-              <ul class="collapse list-unstyled" id="pageSubmenu">
-                <li>
-                    <a href="#">Page 1</a>
-                </li>
-                <li>
-                    <a href="#">Page 2</a>
-                </li>
-                <li>
-                    <a href="#">Page 3</a>
-                </li>
-              </ul>
+              <a href="#">Manage Users</a>
 	          </li>
 	          <li>
-              <a href="#">Portfolio</a>
-	          </li>
-	          <li>
-              <a href="#">Contact</a>
+              <a href="#">Logout</a>
 	          </li>
 	        </ul>
 
 	        <div class="mb-5">
-						<h3 class="h6">Subscribe for newsletter</h3>
-						<form action="#" class="colorlib-subscribe-form">
-	            <div class="form-group d-flex">
-	            	<div class="icon"><span class="icon-paper-plane"></span></div>
-	              <input type="text" class="form-control" placeholder="Enter Email Address">
-	            </div>
-	          </form>
+						
 					</div>
 
 	        <div class="footer">
 	        	<p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-						  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
+						  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved<i class="icon-heart" aria-hidden="true"></i><a href="https://colorlib.com" target="_blank">E</a>
 						  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
 	        </div>
 
@@ -84,15 +76,42 @@
         <!-- Page Content  -->
       <div id="content" class="p-4 p-md-5 pt-5">
 
-        <h2 class="mb-4">Sidebar #02</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <h2 class="mb-4">Welcome, Emmanuel</h2>
+
+
+        <main>
+
+        <!-- dashboard boxes -->
+          <div class="row">
+
+           <div class="col-md-3 dashboard-box mr-5 mb-1 p-4">
+              <div>
+                <h5>Registered Users: 0</h5>
+              </div>
+            </div>
+
+            <div class="col-md-3 dashboard-box mr-5 mb-1 p-4">
+              <div>
+                <h5>Active Users: 0</h5>
+              </div>
+            </div>
+
+            <div class="col-md-3 dashboard-box mr-5 mb-1 p-4">
+              <div>
+                <h5>Total Engagements: 0</h5>
+              </div>
+            </div>
+
+          </div>
+
+        </main>
+        
       </div>
 		</div>
 
-    <script src="js/jquery.min.js"></script>
-    <script src="js/popper.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/main.js"></script>
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/popper.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/main.js"></script>
   </body>
 </html>
